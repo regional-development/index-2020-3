7bf78e674a6da75dd2a5660eb7de267f316e2e7a
hp0404 <hryhorii.pavlenko@gmail.com>
2020-11-11 11:00:13 +0200

    Окремий модуль для підєднання до бази даних - витнятково для scripts/

---
+++ scripts/db_utils.py
@@ -0,0 +1,23 @@
+import os 
+import pandas as pd 
+from sqlalchemy import create_engine
+from dotenv import find_dotenv, load_dotenv
+
+
+load_dotenv(find_dotenv())
+MSSQL = os.environ.get("MSSQL")
+POSTGRESQL = os.environ.get("POSTGRESQL")
+
+
+def db_connect(query, db="MSSQL"):
+    """ Зчитує таблицю з бази даних. 
+    
+    
+    Parameters
+    ----------
+    query : str
+        sql query, напр. SELECT * FROM "Budget"."Incomes";
+    """
+    _db = MSSQL if db == "MSSQL" else POSTGRESQL
+    con = create_engine(_db)
+    return pd.read_sql(query, con)
