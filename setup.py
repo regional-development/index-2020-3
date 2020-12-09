4af4aea4e28f04b344fe175f56a536247ec2e94c
hp0404 <hryhorii.pavlenko@gmail.com>
2020-12-09 20:41:55 +0200

    setup description

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+""" The setup script"""
 from setuptools import find_packages, setup
 
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
 setup(
-    name='src',
+    author="Hryhorii Pavlenko",
+    author_email="hryhorii.pavlenko@gmail.com",
+    name="src",
+    version="0.6",
+    description="Бібліотека для розрахунку індексу оцінки діяльності ОДА",
+    long_description=readme,
+    url="https://index-2020-3.readthedocs.io",
     packages=find_packages(),
-    version='0.1.0',
-    description='Бібліотека для розрахунку індексу оцінки діяльності ОДА',
-    author='hp0404'
 )
